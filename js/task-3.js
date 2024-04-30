0a1eba7f7efdc313288a20db8cc49d15757bd2e3
Tatyana Chuiko <chyikotatyana@gmail.com>
2024-04-30 16:27:23 +0100

    done hw2

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
-function getElementWidth(content, padding, border) {
-  content = Number.parseFloat(content);
-  padding = Number.parseFloat(padding);
-  border = Number.parseFloat(border);
-  const totalWidth = content + 2 * padding + 2 * border;
-  return totalWidth;
+function checkForSpam(message) {
+  const blacklistedWorld1 = 'spam';
+  const blacklistedWorld2 = 'sale';
+  const includesBanWord1 = message.toLowerCase().includes(blacklistedWorld1);
+  const includesBanWord2 = message.toLowerCase().includes(blacklistedWorld2);
+  return includesBanWord1 || includesBanWord2;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
